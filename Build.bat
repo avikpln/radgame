64c78941733c3efd65f2a33a73c0371c33ba563a
Avi Kaplan <savik751@gmail.com>
2026-06-30 11:17:56 +0200

    refactor: restructure project layout
    - Move source files into src/
    - Move UML skeleton files into design/
    - Move radgame.exe into dist/
    - Update Build.bat for new src/ layout
    - Fix -windowed flag bug (was breaking icon embedding)
    - Fix rmdir cleanup step (was passing multiple paths
      to a single rmdir call)
    - Remove hardcoded personal backup path from Build.bat

--- Build.bat
+++ Build.bat
@@ -1,29 +1,27 @@
 @echo off
-rem echo Congratulations! Your first batch file was executed successfully.
-rem pause
+rem Build script: packages the Tkinter GUI into a standalone Windows executable.
+rem Requires PyInstaller (pip install pyinstaller) and Python on PATH.
 
-rem OUTPUT FILES
+SET SRC=src
 SET EXEFILE=radgame.exe
-SET ZIPFILE=radgame-py.zip
+SET DIST=dist
 
 rem PRECLEAN
-if exist %EXEFILE% (
-  del %EXEFILE%
-)
-if exist %ZIPFILE% (
-  del %ZIPFILE%
+if exist %DIST%\%EXEFILE% (
+    del %DIST%\%EXEFILE%
 )
 
 rem BUILD
-python -m PyInstaller --onefile -windowed --name=radgame --icon=radgame.ico radgamegui.py
+cd %SRC%
+python -m PyInstaller --onefile --windowed --name=radgame --icon=radgame.ico radgamegui.py
+cd ..
 
-rem POST CLEAN
-copy dist\%EXEFILE% .
-rmdir /S /Q __pycache__ build dist
-del radgame.spec
+rem MOVE OUTPUT TO dist/
+if not exist %DIST% mkdir %DIST%
+move /Y %SRC%\dist\%EXEFILE% %DIST%\%EXEFILE%
 
-rem ZIP
-"C:\Program Files\7-Zip\7z" a %ZIPFILE% ..\radgame
-
-rem BACKUP
-COPY /Y %ZIPFILE% C:\Users\kavi\Dropbox\avik\__backup__\python\radgame
+rem CLEANUP
+if exist %SRC%\__pycache__ rmdir /S /Q %SRC%\__pycache__
+if exist %SRC%\build rmdir /S /Q %SRC%\build
+if exist %SRC%\dist rmdir /S /Q %SRC%\dist
+if exist %SRC%\radgame.spec del %SRC%\radgame.spec
